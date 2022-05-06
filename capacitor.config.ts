0718832ae3b3f6bc3eb15135334362a09f6f9424
Makktu <johnmcna@gmail.com>
2022-05-06 19:06:30 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'max-effort',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
